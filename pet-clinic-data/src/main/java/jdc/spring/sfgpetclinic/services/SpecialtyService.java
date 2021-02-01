43bd3865633b51d203381328f25bbf5f4d456e7e
JDC <deniscernat@gmail.com>
2021-02-02 00:23:58 +0200

    Added Specialties service map impl. Closes #31

---
+++ pet-clinic-data/src/main/java/jdc/spring/sfgpetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package jdc.spring.sfgpetclinic.services;
+
+import jdc.spring.sfgpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/jdc/spring/sfgpetclinic/services/map/SpecialtyServiceMap.java
@@ -0,0 +1,35 @@
+package jdc.spring.sfgpetclinic.services.map;
+
+import jdc.spring.sfgpetclinic.model.Specialty;
+import jdc.spring.sfgpetclinic.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}
