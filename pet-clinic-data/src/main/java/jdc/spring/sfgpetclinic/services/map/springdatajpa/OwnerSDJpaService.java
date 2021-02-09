77f4400a2f3583f5c3d15c5c522900c02fbf2d89
JDC <deniscernat@gmail.com>
2021-02-09 18:53:33 +0200

    added JPA data service for vets. Closes #41

--- pet-clinic-data/src/main/java/jdc/spring/sfgpetclinic/services/map/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/jdc/spring/sfgpetclinic/services/map/springdatajpa/OwnerSDJpaService.java
@@ -5,12 +5,14 @@ import jdc.spring.sfgpetclinic.repositories.OwnerRepository;
 import jdc.spring.sfgpetclinic.repositories.PetRepository;
 import jdc.spring.sfgpetclinic.repositories.PetTypeRepository;
 import jdc.spring.sfgpetclinic.services.OwnerService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Service
+@Profile("springdatajpa")
 public class OwnerSDJpaService implements OwnerService {
 
     private final OwnerRepository ownerRepository;
