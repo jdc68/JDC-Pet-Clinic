fff42e3d6a73d727d5453567f51ae63818bd285c
JDC <deniscernat@gmail.com>
2021-02-09 18:39:19 +0200

    added JPA data service for owners. Closes #40

--- pet-clinic-data/src/main/java/jdc/spring/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/jdc/spring/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,7 +3,7 @@ package jdc.spring.sfgpetclinic.repositories;
 import jdc.spring.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
-public interface OwnerRepositoty extends CrudRepository<Owner, Long> {
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
-
+    Owner findByLastName(String lastName);
 }
