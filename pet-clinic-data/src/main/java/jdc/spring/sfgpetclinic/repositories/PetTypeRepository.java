fb52e1ed6dd544834f5bf299f7fa8157fb5a2c12
JDC <deniscernat@gmail.com>
2021-02-09 18:59:44 +0200

    added JPA data service for PetTypes. Closes #42

--- pet-clinic-data/src/main/java/jdc/spring/sfgpetclinic/repositories/PetTypeRepository.java
+++ pet-clinic-data/src/main/java/jdc/spring/sfgpetclinic/repositories/PetTypeRepository.java
@@ -1,7 +1,7 @@
 package jdc.spring.sfgpetclinic.repositories;
 
-import jdc.spring.sfgpetclinic.model.Pet;
+import jdc.spring.sfgpetclinic.model.PetType;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PetTypeRepository extends CrudRepository<Pet, Long> {
+public interface PetTypeRepository extends CrudRepository<PetType, Long> {
 }
